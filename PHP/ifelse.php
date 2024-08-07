da576f7ae75d042b02a76a0adbee2210eee23141
annabonvicine <anna.bonvicine.senai404@gmail.com>
2024-08-07 16:37:48 -0300

    07/08

---
+++ PHP/ifelse.php
@@ -0,0 +1,12 @@
+<?php
+//aqui declaramos com a "função"
+$soma1 = 9;
+$soma2 = 10;
+
+if ($soma1 === $soma2) {
+    echo 'Os dois numeros são iguais';
+}else{
+    echo ' Os numeros são diferentes';
+}    
+
+?>
---
+++ PHP/index.php
@@ -0,0 +1,27 @@
+<?php 
+//teste das variaveis
+$nome = 'Anna';
+$cidade = 'Cerquilho';
+$escola = 'SESI Cerquilho';
+$CE = 428;
+$curso = 'Desenvolvimento de Sistemas';
+$media = 9.5;
+
+//chamando as variaveis
+echo 'Olá bem vindo ao SESI-CE <br>';
+
+echo '<br>';
+
+echo "$nome <br>";
+echo "$escola <br>";
+echo "$CE <br>";
+echo "$curso <br>";
+echo "$media <br>";
+
+echo '<br>';
+
+echo "Olá $nome, bem vindo ao SESI de $cidade, sua nota no curso de $curso é: $media";
+
+
+
+?>
