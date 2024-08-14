978e15ae1ce817482432444c27803311dc3705af
annabonvicine <anna.bonvicine.senai404@gmail.com>
2024-08-14 16:26:50 -0300

    14/08

---
+++ PHP/operadores_ternários.php
@@ -0,0 +1,21 @@
+<?php
+
+//exemplo com variavel numerico
+$idade = 20;
+$resultado = ($idade >= 18) ? "Maior de idade" : "Menor de idade";
+echo $resultado; //saída: Maior de idade
+
+echo "<br/>";
+
+//aqui nesse bloco sera feito com um nome
+$nome = "Anna";
+$mensagem = ($nome == "Anna") ? "Olá, Anna!" : "Olá, visitante!";
+echo $mensagem; //No site vai parecer "Olá, Anna!"
+
+echo "<br/>";
+
+$is_logged_in = true;
+$mensagem = $is_logged_in ? 'Bem-vindo de volta!' : 'Por favor, faça login!';
+echo $mensagem;
+
+?>
