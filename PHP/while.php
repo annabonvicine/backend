4c588fddfaaf04f0b76255a6b74301972a2ad8ad
annabonvicine <anna.bonvicine.senai404@gmail.com>
2024-09-20 10:39:22 -0300

    while.php

--- PHP/while.php
+++ PHP/while.php
@@ -1,7 +1,7 @@
 <?php
-
+//aqui foi declarado o contador
 $contador = 1;
-
+//enquanto o contador for menor que 5 ele vai aparecer
 while($contador <= 5){
 
     echo "O contador é: $contador <br>";
