29bab1a0a9c938cb47776cab5c09e7064bf00d0b
annabonvicine <anna.bonvicine.senai404@gmail.com>
2024-10-02 13:52:07 -0300

    02/10

---
+++ HTML_PHP/index.php
@@ -0,0 +1,23 @@
+<?php
+$nome = 'Anna';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+  <h1>Bem-vindo(a)
+  <?php echo $nome; 
+  ?></h1>  
+ 
+  <?php
+  echo '<h1> Olá meu SENAI';
+  ?>
+
+</body>
+</html>
